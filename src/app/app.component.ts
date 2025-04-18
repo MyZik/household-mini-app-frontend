5a9e0e8019617f84fda1283fa0142e69c9b4c232
Eldar Gazaliev <eldarqa@gmx.de>
2025-04-18 21:15:44 +0200

    [main] Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {TelegramWebApp} from "@m1cron-labs/ng-telegram-mini-app";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'telegram-webapp-angular';
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly telegram = inject(TelegramWebApp);
+
+  user = this.telegram.initDataUnsafe?.user;
+
+  constructor() {
+    this.telegram.ready();
+  }
+
+  ngOnInit() {
+    console.debug('Telegram Web App is ready', this.telegram.initDataUnsafe);
+  }
+
+  ngOnDestroy(): void {
+    this.telegram.close();
+  }
+
+  closeApp(): void {
+    this.telegram.close();
+  }
 }
